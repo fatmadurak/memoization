ba287bb1a1e437ae470d92bef04e96b7c5e5c1da
fatmadurak <fatmabadillidurak@gmail.com>
2023-01-07 21:18:13 +0300

    commited

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-function List() {
-  return (
-    <div>List</div>
-  )
-}
+const List = ({ userList, handleClear }) => {
+    console.log("Render: List")
 
-export default List
+    return (
+        <div>
+            <ul style={{ display: 'flex', listStyle: 'none' }}>
+                {userList.map(item => (
+                    <ListItem key={item.id} item={item} />
+                ))
+                }
+            </ul>
+        </div>
+    );
+};
+
+const ListItem = React.memo(props => {
+    console.log("Render: ListItem")
+    return <li style={{ border: '1px solid black', padding: 20 }}>{props.item.name}</li>
+})
+
+export default React.memo(List);
